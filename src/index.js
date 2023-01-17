27eb2f691f19a5e13017a8b5b7d8a7a2844133db
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-01-16 20:20:54 -0500

    included browserrouter

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+    </BrowserRouter>
 );
