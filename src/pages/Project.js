088682bfb8f3b57692bfbd3fae38c19abc63b2dc
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-01-16 21:51:24 -0500

    modified github links

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import 'animate.css';
 import '../App.css';
-import { useState } from 'react';
 import { Contact } from '../components/Contact';
 import Stack from 'react-bootstrap/Stack';
 import { Copyright } from '../components/Copyright';
@@ -11,22 +10,21 @@ export const Project = () => {
   const shop = require('../img/shop.jpeg');
   const reversi = require('../img/reversi.png');
   const ucharity = require('../img/ucharity.png');
-  const [click, setClick] = useState(false);
-  const [click2, setClick2] = useState(false);
-  const [click3, setClick3] = useState(false);
-  const [click4, setClick4] = useState(false);
 
-  const handleClick = () => {
-    setClick(!click);
+  const openShop = () => {
+    return window.open('https://github.com/daisyyedda/Online-Shop-App.git', "_blank");
   }
-  const handleClick2 = () => {
-    setClick2(!click2);
+
+  const openCC3K = () => {
+    return window.open('https://github.com/daisyyedda/ChamberCrawler3000.git', "_blank");
   }
-  const handleClick3 = () => {
-    setClick3(!click3);
+
+  const openReversi = () => {
+    return window.open('https://github.com/daisyyedda/Reversi.git', "_blank");
   }
-  const handleClick4 = () => {
-    setClick4(!click4);
+
+  const openUCharity = () => {
+    return window.open('https://github.com/daisyyedda/UCharity.git', "_blank");
   }
 
   return (
@@ -36,25 +34,19 @@ export const Project = () => {
       <Stack gap={200}>
         <div className="bg-light border">
           <h3 className='pos'>Online Shop App (Node.js /  EJS  /  MongoDB)</h3>
-          <img className="project" alt="Online Shop App Snapshot" src={shop} onClick={handleClick}></img> 
-          {click ? 
-          window.open('https://github.com/daisyyedda/Online-Shop-App.git', "_self")
-           : null}
+          <img className="project" alt="Online Shop App Snapshot" src={shop} onClick={openShop}></img> 
         </div>
         <div className="bg-light border">
           <h3 className='pos'>Game of ChamberCrawler3000+ (C++) </h3>
-          <img className= "project" alt="CC3k+ Snapshot" src={cc3k} onClick={handleClick2}></img>
-          {click2 ? window.open("https://github.com/daisyyedda/ChamberCrawler3000.git", "_self") : null}
+          <img className= "project" alt="CC3k+ Snapshot" src={cc3k} onClick={openCC3K}></img>
         </div>
         <div className="bg-light border">
           <h3 className='pos'> Game of Reversi (C++)</h3>
-          <img className="project" alt="Reversi Snapshot" src={reversi} onClick={handleClick3}></img>
-          {click3 ? window.open("https://github.com/daisyyedda/Reversi.git", "_self") : null}      
+          <img className="project" alt="Reversi Snapshot" src={reversi} onClick={openReversi}></img>
         </div>
         <div className="bg-light border">
           <h3 className='pos'>UCharity Website (HTML / CSS / JavaScript) </h3> 
-          <img className="project" alt="UCharity Logo" src={ucharity} onClick={handleClick4}>
-            {click4 ? window.open("https://github.com/daisyyedda/UCharity.git", "_self") : null}
+          <img className="project" alt="UCharity Logo" src={ucharity} onClick={openUCharity}>
           </img>
         </div>
       </Stack>
