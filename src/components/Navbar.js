037d6113e3a8cdebd5de9bd3db4df51521e329e5
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-01-23 15:38:44 -0500

    onClick effect for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const underLine = (e) => {
+    const links = document.querySelectorAll(".links a");
+    links.forEach((link) => {
+      link.style.borderBottom = "none";
+    });
+    e.target.style.borderBottom = "2px solid white";
+  }
 
   return (  
     <div className="navbar">
       <div className="links">
-        <Link to="/" style={{color: "white", textDecoration: "none"}}> Home </Link>
+        <Link to="/" style={{color: "white", textDecoration: "none"}} onClick={underLine}> Home </Link>
         &nbsp;&nbsp;&nbsp;
-        <Link to="/coop" style={{color: "white", textDecoration: "none"}}> Internships </Link>
+        <Link to="/coop" style={{color: "white", textDecoration: "none"}} onClick={underLine}> Internships </Link>
         &nbsp;&nbsp;&nbsp;
-        <Link to="/project" style={{color: "white", textDecoration: "none"}}> Projects </Link>
+        <Link to="/project" style={{color: "white", textDecoration: "none"}} onClick={underLine}> Projects </Link>
         &nbsp;&nbsp;&nbsp;
-        <Link to="/mathie" style={{color: "white", textDecoration: "none"}}> Mathie? </Link>
+        <Link to="/mathie" style={{color: "white", textDecoration: "none"}} onClick={underLine}> Mathie? </Link>
       </div>
     </div>
   );
